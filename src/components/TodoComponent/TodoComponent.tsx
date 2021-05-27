f61ffea8a724b1b15c29ae5eff7cb28c64125749
Mark Marincek <mark.marincek@styliff.com>
2021-05-27 13:25:23 +0200

    Prepared code for demo

--- src/components/TodoComponent/TodoComponent.tsx
+++ src/components/TodoComponent/TodoComponent.tsx
@@ -4,13 +4,13 @@ import { TodoWrapper } from './TodoComponent.styles';
 
 interface TodoComponentProps {
   todo: Todo;
-  onClick: (todo: Todo) => void;
+  onClick: () => void;
 }
 function TodoComponent(props: TodoComponentProps) {
   const { todo, onClick } = props;
   console.log('render');
   return (
-    <TodoWrapper $done={todo.completed} onClick={() => onClick(todo)}>
+    <TodoWrapper $done={todo.completed} onClick={onClick}>
       <p>{todo.title}</p>
     </TodoWrapper>
   );
--- src/pages/BasicHooks.tsx
+++ src/pages/BasicHooks.tsx
@@ -1,57 +1,17 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React from 'react';
 import Container from '../components/Container/Container';
 import Input from '../components/Input';
 import Row from '../components/Row';
-import TodoComponent from '../components/TodoComponent';
-import ToggleSwitch from '../components/ToggleSwitch';
-import { loadTodos } from '../utils/api';
-import { Todo } from '../utils/interfaces';
-import { useBackgroundColor, useWidth } from '../utils/hooks';
 
 const BasicHooks = () => {
-  const [name, setName] = useState('test');
-  const [todos, setTodos] = useState<Todo[] | undefined>(undefined);
-  const [animateBackground, setAnimateBackground] = useState(true);
-
-  const width = useWidth();
-  const backgroundColor = useBackgroundColor(animateBackground);
-
-  useEffect(() => {
-    document.title = name;
-  });
-
-  useEffect(() => {
-    loadTodos().then((todos) => setTodos(todos));
-  }, []);
-
-  const onTodoClick = useCallback((todo: Todo) => {
-    setTodos((state) =>
-      state?.map((t) => {
-        if (t.id !== todo.id) return t;
-        return {
-          ...t,
-          completed: !t.completed,
-        };
-      })
-    );
-  }, []);
-
-  function handleNameChange(event: React.ChangeEvent<HTMLInputElement>) {
-    setName(event.currentTarget.value);
-  }
-
   return (
-    <Container $color={backgroundColor}>
-      <Row label={'name'}>
-        <Input onChange={handleNameChange} value={name} />
+    <Container>
+      <Row label="name">
+        <Input value="Mark" />
       </Row>
-      <Row label="Animated background">
-        <ToggleSwitch checked={animateBackground} onChange={setAnimateBackground} />
+      <Row label="surname">
+        <Input value="Marincek" />
       </Row>
-      <Row label={String(width)} />
-      {todos?.map((todo) => (
-        <TodoComponent key={todo.id} todo={todo} onClick={onTodoClick} />
-      ))}
     </Container>
   );
 };
